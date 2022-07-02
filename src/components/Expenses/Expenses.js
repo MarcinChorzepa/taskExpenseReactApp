c8a29a2207dedcfa24561b40d0cea35ba8836382
Chorzępa <marcin.chorzpa>
2022-07-02 13:35:07 +0200

    change filter year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import ExpensesChart from "./ExpensesChart";
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const selectionChangeHandler = (event) => {
     setFilteredYear(event);
